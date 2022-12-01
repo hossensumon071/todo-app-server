0d211ab6888df0c074afc9a17a059b8945641d9e
hossensumon071 <55638643+hossensumon071@users.noreply.github.com>
2022-12-01 18:38:46 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// import {jsonserver} from 'json-server' (this style use for only Front end);
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.port || 5000
+
+server.use(middlewares);
+server.use(router);
+server.listen(port)
